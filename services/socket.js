2ce900b527da4f0552bcf5361a3282f2799c4b77
Abdur <ubaidrao1212@gmail.com>
2026-05-19 16:53:26 +0500

    update CORS origin for socket server

--- services/socket.js
+++ services/socket.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 // <= SOCKET SERVER INSTANCE =>
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "https://feedmill-client.vercel.app"],
+    origin: ["http://localhost:5173", "https://feedmill-pro.vercel.app"],
     methods: ["GET", "POST"],
   },
 });
