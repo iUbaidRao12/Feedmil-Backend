57d3f37813ba396ccdd885f63d8c6b83dd4b743c
Abdur <ubaidrao1212@gmail.com>
2026-05-19 17:10:16 +0500

    update allowed origins for CORS configuration

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,7 +3,7 @@ const allowedOrigins = [
   // FRONTEND LOCALHOST
   "http://localhost:5173",
   "http://localhost:5174",
-  "https://feedmill-client.vercel.app",
+  "https://feedmill-pro.vercel.app",
 ];
 
 export default allowedOrigins;
